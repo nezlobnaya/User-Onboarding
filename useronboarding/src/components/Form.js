c5739187324b03cf6187b3c479ebcd9e36caa8aa
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-09-11 11:30:59 -0400

    created all the fields in the Form component

--- useronboarding/src/components/Form.js
+++ useronboarding/src/components/Form.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+import {Form, Field, withFormik } from 'formik';
 
 
-const Form = () => {
+
+
+const UserForm = ({ errors, touched, values }) => {
     return ( 
-        <div className='form'>
-
+        <div className='user-form'>
+         <Form>
+            <h1>Fill out the User Form!</h1>
+            <Field name='name' type='text' placeholder='name'></Field>
+            <Field name='email' type='email' placeholder='email'></Field>
+            <Field name='password' type='password' placeholder='password'></Field>
+            <textarea>Terms of Service</textarea>
+            <label className='checkbox-container'>
+                I accept & agree to the Terms of Service
+            <Field 
+            type='checkbox'
+            name='checkbox'
+            checked ={values.checkbox} ></Field>
+            </label>
+            
+             <button>Submit</button>
+         </Form>
         </div>
      );
 }
+ const FormikUserForm = withFormik({
+
+    mapPropsToValues({ name, email, password, textarea, checkbox }) {
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            checkbox: checkbox || false
+        };
+    }
+
+ })(UserForm)
  
-export default Form ;
+export default FormikUserForm;
